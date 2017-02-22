e31cc0b8724fb3f105a65d6e3a61733b7dd7e3d6
Shaun Keenan <skeenan@gmail.com>
2017-02-21 20:58:12 -0800

    ssh key perms

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,5 +2,6 @@
 [ -d ~/.ssh ] || mkdir ~/.ssh
 openssl aes-256-cbc -K $encrypted_b293f50235e4_key -iv $encrypted_b293f50235e4_iv \
     -in id_travis.enc -out ~/.ssh/id_rsa -d
+chmod 0600 ~/.ssh/id_rsa
 scp -p scripts/run.sh skeenan@skeenan.net:/tmp/run-alexa-srvbc.sh
 ssh skeenan@skeenan.net /tmp/run-alexa-srvbc.sh $REPO:$TAG
