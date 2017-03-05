350b18f9b42813bfd178d47df2619436fdaf8aeb
Shaun Keenan <skeenan@gmail.com>
2017-03-04 22:16:28 -0800

    deploy scripts use docker-compose

--- scripts/run.sh
+++ scripts/run.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-IMAGE=$1
-docker pull $IMAGE
-docker pull redis
-docker run -d --name redis -p 6379:6379 redis
-docker rm -f alexa-srvbc
-docker run -td -e RAILS_ENV=production -p127.0.0.1:9899:5000 --name=alexa-srvbc $IMAGE
+cd /tmp
+RAILS_ENV=production docker-compose -d up
